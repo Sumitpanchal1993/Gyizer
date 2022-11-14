917548a2705a83e6fee9bf0175ab817e4474868a
Sumitpanchal1993 <Sumitpanchal08@gmail.com>
2022-11-14 11:04:39 +0530

    final changes done

---
+++ index.js
@@ -0,0 +1,9 @@
+// to toggle on small screens
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
